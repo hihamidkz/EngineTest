a8fc7fdb10de2a88fe99002e29d0a8bbd42ae4a5
Хамидулла <hkasimov1998@inbox.ru>
2019-07-19 14:43:56 +0700

    Edited testing logic

--- EngineTest/TestStand.h
+++ EngineTest/TestStand.h
@@ -6,7 +6,7 @@
 
 #include "Engine.h"
 
-#define EPS 1e-1
+#define EPS 1e-5
 
 template <class T>
 class TestStand {
@@ -21,21 +21,22 @@ public:
 
 	int startTest() {
 		int time;
-		double a;
+		double curT = ambT; // Engine's current temperature
 		double prev = 0;
-		double curV = (*engine->chart.begin()).first; // Current speed
-		double curM = (*engine->chart.begin()).second; // Current torque
 
-		for (time = 0; engine->tP - engine->t > EPS; time++) {
-			double up = curM  * engine->hM + pow(curV, 2) * engine->hV;
-			double down = engine->c * (engine->t - ambT);
-			engine->t += up;
-			engine->t -= down;
+		engine->start();
 
-			a = curM / engine->i;
-			curV += a;
-			curM = interpolate(engine->chart, curV);
-			prev = engine->t;
+		for (time = 0; engine->getTP() - engine->getT() > EPS; time++) {
+			if (curT == prev) {
+				return -1;
+			}
+
+			curT = engine->getT();
+			engine->warming();
+			engine->cooling(curT, ambT);
+
+			engine->timestamp();
+			prev = engine->getT();
 		}
 
 		return time;
@@ -43,24 +44,6 @@ public:
 private:
 	T *engine;
 	double ambT; // Ambient temperature
-
-	/*
-	  Using Linear Interpolation to calculate 
-	  the current torque depending on speed
-	*/
-	double interpolate(std::map<double, double> mas, double val) {
-		auto dot1 = mas.lower_bound(val);
-		auto dot2 = prev(dot1);
-		double x = (*dot1).first;
-		double xPrev = (*dot2).first;
-		double y = (*dot1).second;
-		double yPrev = (*dot2).second;
-
-		double a = (y - yPrev) / (x - xPrev);
-		double b = yPrev - a * xPrev;
-
-		return a * val + b;
-	}
 };
 
 #endif
